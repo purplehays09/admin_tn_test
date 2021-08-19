254a845c50425c120ac684c4264acecd2623ccec
purplehays09 <purplehays527@gmail.com>
2021-08-19 13:27:47 -0600

    Added user_spec and create_user migration
    I added the user spec and the basic valid with valid attributes portion. I then created the migration. I will create the model after this.
    
    Next:
     - Create the user model and test

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe User do
+  subject { 
+    User.new(
+      email:'david.hays@tuftandneedle.com',
+      password:'guest'
+    )
+   } 
+
+   it "is valid with valid attributes" do
+     expect(subject).to be_valid 
+   end
+   
+end
